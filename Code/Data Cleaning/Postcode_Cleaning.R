35ed2257dd6594352f7639a219705a80ca5ff29b
SambandhaRai <ginny_30@icloud.com>
2025-07-26 14:39:35 +0545

    Data Cleaning

--- Code/Data Cleaning/Postcode_Cleaning.R
+++ Code/Data Cleaning/Postcode_Cleaning.R
@@ -1,5 +1,19 @@
 library(tidyverse)
 library(dplyr)
 
-Postcode_raw = read_csv("Obtained Data/Postcode/Postcode to LSOA.csv")
-View(Postcode_raw)
+Town_cleaned = read_csv("Cleaned Data/Towns.csv")
+
+Postcode_LSOA = read_csv("Obtained Data/Postcode/Postcode to LSOA.csv")
+
+pattern = ' .*$'
+
+Postcode_LSOA_Cleaned = Postcode_LSOA %>%
+  select(lsoa11cd, pcds) %>% 
+  mutate(shortPostcode = gsub(pattern,"",pcds)) %>% 
+  right_join(Town_cleaned, by = "shortPostcode")  %>% 
+  group_by(lsoa11cd) %>% 
+  select(`LSOA code` = lsoa11cd, shortPostcode, Town, District, County) 
+
+View(Postcode_LSOA_Cleaned)
+
+write.csv(Postcode_LSOA_Cleaned, "Cleaned Data/cleanedPostcode_LSOA.csv")
